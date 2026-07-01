91abeb4fd5de48fb4431c8433403aefeb321eb86
masud51435 <masudrana82955@gmail.com>
2026-07-01 11:31:41 +0600

    Clean up routes and integrate ProfileScreen
    - Remove unused placeholder routes and constants for onboarding, OTP verification, and various commerce-related screens.
    - Replace the `profileScreen` placeholder with the actual `ProfileScreen` widget.
    - Update the `HomeScreen` import path to reflect the current project structure.

--- lib/app/routes/routes_config.dart
+++ lib/app/routes/routes_config.dart
@@ -3,22 +3,14 @@ import 'package:flutter/material.dart';
 import '../../features/auth/presentation/login/view/login_screen.dart';
 import '../../features/auth/presentation/sign_up/view/signup_screen.dart';
 import '../../features/dashboard/view/dashboard_screen.dart';
-import '../../features/home/presentation/home/view/home_screen.dart';
+import '../../features/home/presentation/view/home_screen.dart';
+import '../../features/user_profile/presentation/view/profile_screen.dart';
 
 class RoutesConfig {
   static const splash = Center(child: Text('Splash'));
-  static const onBoarding = Center(child: Text('Onboarding'));
   static const login = LoginScreen();
   static const signUp = SignUpScreen();
-  static const verifyOtp = Center(child: Text('Verify OTP'));
   static final dashboard = DashboardScreen();
   static const homeScreen = HomeScreen();
-  static const profileScreen = Center(child: Text('Profile'));
-  static const productDetailScreen = Center(child: Text('Product Detail'));
-  static const virtualCard = Center(child: Text('Virtual Card'));
-  static const addMoney = Center(child: Text('Add Money'));
-  static const myOrder = Center(child: Text('My Order'));
-  static const cart = Center(child: Text('Cart'));
-  static const wishlist = Center(child: Text('Wishlist'));
-  static const settings = Center(child: Text('Settings'));
+  static const profileScreen = ProfileScreen();
 }
